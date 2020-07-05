76b1178f2f00c3d892a968e7d8b0ba9337de2fe6
Matthew Ma <monsteryolo123@gmail.com>
2020-07-05 10:07:38 -0400

    File Operations

---
+++ Fundamentals/2.1 Variables.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    char my_grade = 'a';
+    cout << int(my_grade) << endl;
+
+    char a = 95, b = 80, c = 75;
+    int char_num = 100;
+
+    cout << a << endl;
+    cout << char(char_num) << endl;
+
+    
+    string str = "你好！";
+    cout << str << endl;
+
+    
+    string chinese_str = "你";
+    cout << chinese_str << endl;
+
+    char chinese_c = '你';
+    cout << chinese_c << endl;
+
+    return 0;
+
+}
---
+++ Fundamentals/2.5 File Operations.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ofstream fout ("file.out");
+
+    string str = "haha";
+    fout << str;
+
+    int x = 513;
+    fout << x;
+
+    return 0;
+
+}
+
